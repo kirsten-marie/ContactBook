0636603b459e733471b8a0bbd02d1de620c292ac
Kirsten McCain <KMcCain@edfinancial.com>
2022-08-21 22:49:38 -0400

    Updated address controller

--- ContactBook/Controllers/AddressController.cs
+++ ContactBook/Controllers/AddressController.cs
@@ -5,5 +5,23 @@ namespace ContactBook.Controllers;
 public class AddressController : ControllerBase
 {
     private readonly IAddressService _addressService;
-    
+
+    public AddressController(IAddressService addressService) => _addressService = addressService;
+
+    [HttpGet]
+    public ActionResult<List<Address>> GetAllAddresses() => 
+        _addressService.GetAddresses().ToList();
+
+    [HttpGet]
+    public ActionResult<List<State>> GetAllStates() => 
+        _addressService.GetStates().ToList();
+
+    [HttpPost]
+    public IActionResult Create(Address address)
+    {
+       _addressService.CreateAddress(address);
+       return CreatedAtAction(nameof(Create), new { id = address.AddressId}, address);
+    }
+
+
 }
